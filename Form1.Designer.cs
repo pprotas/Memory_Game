4d915ed206423afe6fd713bd2390e5bc9129bb8f
Pawel Protas <pawprotas@gmail.com>
2017-10-11 18:18:16 +0200

    Versie 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Memory_Project
+{
+    partial class Start_Screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start_Screen));
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.Location = new System.Drawing.Point(100, 100);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(90, 50);
+            this.Start_Button.TabIndex = 0;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = true;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Start_Screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.Start_Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Start_Screen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Project
+{
+    public partial class Start_Screen : Form
+    {
+        bool GridInit = false;
+
+        Form Game_Screen = new Form();
+
+        Random rng = new Random();
+
+        List<Image> icons = new List<Image>();
+
+        PictureBox firstClicked = null;
+        PictureBox secondClicked = null;
+
+        public Start_Screen()
+        {
+            InitializeComponent();
+
+            this.CenterToScreen();
+
+            #region Foto's
+            icons.Add(Image.FromFile(@"./imgs/Cyan.png"));
+            icons.Add(Image.FromFile(@"./imgs/Cyan.png"));
+            icons[0].Tag = "0";
+            icons[1].Tag = "0";
+            icons.Add(Image.FromFile(@"./imgs/LBlue.png"));
+            icons.Add(Image.FromFile(@"./imgs/LBlue.png"));
+            icons[2].Tag = "1";
+            icons[3].Tag = "1";
+            icons.Add(Image.FromFile(@"./imgs/LGreen.png"));
+            icons.Add(Image.FromFile(@"./imgs/LGreen.png"));
+            icons[4].Tag = "2";
+            icons[5].Tag = "2";
+            icons.Add(Image.FromFile(@"./imgs/Orange.png"));
+            icons.Add(Image.FromFile(@"./imgs/Orange.png"));
+            icons[6].Tag = "3";
+            icons[7].Tag = "3";
+            icons.Add(Image.FromFile(@"./imgs/Pink.png"));
+            icons.Add(Image.FromFile(@"./imgs/Pink.png"));
+            icons[8].Tag = "4";
+            icons[9].Tag = "4";
+            icons.Add(Image.FromFile(@"./imgs/Purple.png"));
+            icons.Add(Image.FromFile(@"./imgs/Purple.png"));
+            icons[10].Tag = "5";
+            icons[11].Tag = "5";
+            icons.Add(Image.FromFile(@"./imgs/Red.png"));
+            icons.Add(Image.FromFile(@"./imgs/Red.png"));
+            icons[12].Tag = "6";
+            icons[13].Tag = "6";
+            icons.Add(Image.FromFile(@"./imgs/Yellow.png"));
+            icons.Add(Image.FromFile(@"./imgs/Yellow.png"));
+            icons[14].Tag = "7";
+            icons[15].Tag = "7";
+            #endregion Foto's 
+        }
+
+        private void Game_Screen_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                Start_Button.Enabled = true;
+                e.Cancel = true;
+                Game_Screen.Hide();
+            }
+        }
+
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            Start_Button.Enabled = false;
+
+            Game_Screen.Text = "Memory Game";
+            Game_Screen.Icon = this.Icon;
+            Game_Screen.StartPosition = FormStartPosition.CenterScreen;
+            Game_Screen.Size = new Size(440, 665);
+            Game_Screen.Show();
+            Game_Screen.FormClosing += new FormClosingEventHandler(Game_Screen_FormClosing);
+
+            if (GridInit == false)
+            {
+                InitGrid();
+            }
+        }
+
+        private void Card_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedPic = sender as PictureBox;
+            clickedPic.Enabled = false;
+            clickedPic.Image = null;
+            if(firstClicked == null)
+            {
+                firstClicked = clickedPic;
+                return;
+            }
+
+            if(firstClicked != null && secondClicked == null)
+            {
+                secondClicked = clickedPic;
+                if (firstClicked.BackgroundImage.Tag != secondClicked.BackgroundImage.Tag)
+                {
+                    foreach(PictureBox pic in Game_Screen.Controls)
+                    {
+                        pic.Enabled = false;
+                    }
+                    timer1.Start();
+                }
+                else
+                {
+                    foreach(PictureBox pic in Game_Screen.Controls)
+                    {
+                        if(pic.Image == null)
+                        {
+                            pic.Enabled = false;
+                        }
+                    }
+                    CheckWinner();
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+                return;
+            }   
+        }
+
+        void CheckWinner()
+        {
+            foreach(PictureBox pic in Game_Screen.Controls)
+            {
+                if(pic.Image != null)
+                {
+                    return;
+                }
+            }
+
+            MessageBox.Show("Je hebt gewonnen!", "Gefeliciteerd");
+            Game_Screen.Close();
+            #region Foto's
+            icons.Add(Image.FromFile(@"./imgs/Cyan.png"));
+            icons.Add(Image.FromFile(@"./imgs/Cyan.png"));
+            icons[0].Tag = "0";
+            icons[1].Tag = "0";
+            icons.Add(Image.FromFile(@"./imgs/LBlue.png"));
+            icons.Add(Image.FromFile(@"./imgs/LBlue.png"));
+            icons[2].Tag = "1";
+            icons[3].Tag = "1";
+            icons.Add(Image.FromFile(@"./imgs/LGreen.png"));
+            icons.Add(Image.FromFile(@"./imgs/LGreen.png"));
+            icons[4].Tag = "2";
+            icons[5].Tag = "2";
+            icons.Add(Image.FromFile(@"./imgs/Orange.png"));
+            icons.Add(Image.FromFile(@"./imgs/Orange.png"));
+            icons[6].Tag = "3";
+            icons[7].Tag = "3";
+            icons.Add(Image.FromFile(@"./imgs/Pink.png"));
+            icons.Add(Image.FromFile(@"./imgs/Pink.png"));
+            icons[8].Tag = "4";
+            icons[9].Tag = "4";
+            icons.Add(Image.FromFile(@"./imgs/Purple.png"));
+            icons.Add(Image.FromFile(@"./imgs/Purple.png"));
+            icons[10].Tag = "5";
+            icons[11].Tag = "5";
+            icons.Add(Image.FromFile(@"./imgs/Red.png"));
+            icons.Add(Image.FromFile(@"./imgs/Red.png"));
+            icons[12].Tag = "6";
+            icons[13].Tag = "6";
+            icons.Add(Image.FromFile(@"./imgs/Yellow.png"));
+            icons.Add(Image.FromFile(@"./imgs/Yellow.png"));
+            icons[14].Tag = "7";
+            icons[15].Tag = "7";
+#endregion Foto's 
+
+            Start_Button.Enabled = true;
+        }
+        void InitGrid()
+        {
+            GridInit = true;
+
+            int x = 5;
+            int y = 5;
+
+            for(int r = 0; r < 4; r++)
+            {
+                for(int c = 0; c < 4; c++)
+                {
+                    PictureBox card = new PictureBox();
+                    int rngNum = rng.Next(icons.Count);
+                    card.BackgroundImage = icons[rngNum];
+                    icons.RemoveAt(rngNum);
+                    card.Image = Image.FromFile(@"./imgs/back.png");
+                    card.Size = new Size(100, 150);
+                    card.Location = new Point(x, y);
+                    card.Cursor = Cursors.Hand;
+                    card.Click += new EventHandler(this.Card_Click);
+
+                    Game_Screen.Controls.Add(card);
+
+                    x += 105;
+                }
+                x = 5;
+                y += 155;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach(PictureBox pic in Game_Screen.Controls)
+            {
+                if (pic.Image != null)
+                {
+                    pic.Enabled = true;
+                }
+            }
+
+            timer1.Stop();
+            firstClicked.Image = Image.FromFile(@"./imgs/back.png");
+            secondClicked.Image = Image.FromFile(@"./imgs/back.png");
+            firstClicked.Enabled = true;
+            firstClicked = null;
+            secondClicked.Enabled = true;
+            secondClicked = null;
+        }
+    }
+}
